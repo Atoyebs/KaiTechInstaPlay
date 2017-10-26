420f2b70e8c2bf6dc2b900b552ffa9bc3373eeae
Ini Atoyebi <ato_no2@hotmail.co.uk>
2017-10-26 15:09:11 +0100

    gotten the Instagram text logo showing on the screen/background as well as created the skeleton for the LoginButton component

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import {Text, View, ScrollView, TouchableHighlight, Image, StatusBar } from 'react-native';
+import Dimensions from 'Dimensions';
+
+//this code creates a constant that holds the dimensions of the current device as an object
+const window = Dimensions.get('window');
 
 export default class App extends React.Component {
   render() {
     return (
-      <Image source={require('./src/images/insta_login_background.jpg')}>
+      <Image source={require('./src/images/insta_login_background.jpg')} style={viewStyles.container}>
 
         <StatusBar backgroundColor="transparent" barStyle="light-content" />
 
-        
+        <ScrollView style={viewStyles.ScrollView}>
+
+          <Image source={require('./src/images/instagram-text-logo.png')}
+           resizeMode={'contain'}
+           style={viewStyles.instagramTextLogo}
+          />
+
+        </ScrollView>
 
       </Image>
     );
@@ -19,9 +30,20 @@ const viewStyles = {
 
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    width: null,
+    height: null
+  },
+  scrollView: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  instagramTextLogo: {
+    width: 145,
+    height: 55,
+    marginTop: (window.height * 0.20),
+    marginBottom: 30
   }
 
 };
---
+++ src/components/LoginButton.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {View, TouchableHighlight, Text} from 'react-native';
+
+
+class LoginButton extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+    return(
+      <TouchableHighlight>
+
+      </TouchableHighlight>
+    );
+
+  }
+
+
+}
+
+
+export default LoginButton;
